c17dd7391a70079e4cc72d11b69e4cdf8cce2c69
Tanvir Ahmed <100063654+Tanvir793181@users.noreply.github.com>
2025-04-24 14:10:08 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const serverless = require('serverless-http');
 const app = express();
 
 // Connect database
@@ -29,6 +30,6 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Server Error' });
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports.handler = serverless(app);
+//const PORT = process.env.PORT || 5000;
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
